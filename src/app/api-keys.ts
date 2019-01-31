8cc4805ddbcfc68bb4dda1880b8a0e27138f7fce
Student <example@example.com>
2019-01-31 15:35:33 -0800

    innitial and final commit.

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+
+  export const config = {
+    apiKey: "AIzaSyA7Q5WL8YQK3yaYKv7AhBX4ke8PojovBxc",
+    authDomain: "authentication-example-73f8b.firebaseapp.com",
+    databaseURL: "https://authentication-example-73f8b.firebaseio.com",
+    projectId: "authentication-example-73f8b",
+    storageBucket: "authentication-example-73f8b.appspot.com",
+    messagingSenderId: "70540863013"
+  };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { config } from './api-keys';
+import { PublicComponent } from './public/public.component';
+import { PrivateComponent } from './private/private.component';
 
+export const firebaseConfig = {
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    PrivateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
